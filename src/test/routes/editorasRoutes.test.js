c818c31a8cd0c4f8c54b281631e854cfe5e21051
Raí Joia <megac154@gmail.com>
2024-03-12 10:09:54 -0300

    feat: testando rota /editoras com package supertest

--- src/test/routes/editorasRoutes.test.js
+++ src/test/routes/editorasRoutes.test.js
@@ -1,7 +1,12 @@
-import { beforeEach, afterEach } from '@jest/globals';
+import {
+  beforeEach, afterEach, describe, expect, it,
+} from '@jest/globals';
+import request from 'supertest';
 import app from '../../app.js';
 
 let server;
+let idResposta;
+
 beforeEach(() => {
   const PORT = 3000;
   server = app.listen(PORT);
@@ -9,4 +14,43 @@ beforeEach(() => {
 
 afterEach(() => {
   server.close();
+});
+
+describe('GET /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+describe('POST /editoras', () => {
+  it('Deve criar uma nova editora', async () => {
+    const reposta = await request(app)
+      .post('/editoras')
+      .send({ nome: 'Raí', email: 'rai@rai.com', cidade: 'São Paulo' })
+      .expect(201);
+
+    idResposta = reposta.body.content.id;
+  });
+});
+
+describe('GET /editoras/id', () => {
+  it('Deve retornar o recurso selecionado', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('DELETE /editoras/id', () => {
+  it('Deve deletar uma editora', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
+      .expect(200);
+  });
 });
